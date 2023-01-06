442577205cccb749741f17a8558672a6c01fcfe2
AmrMohamed26 <105735501+AmrMohamed26@users.noreply.github.com>
2023-01-06 23:07:05 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+import plugin from "tailwindcss/plugin";
+
+const Myclass = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".my-rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px",
+    },
+    ".backface-hidden": {
+      backfaceVisibility: "hidden",
+    },
+  });
+});
+export const content = ["./src/**/*.{html,js}"];
+export const theme = {
+  extend: {},
+};
+export const plugins = [Myclass];
